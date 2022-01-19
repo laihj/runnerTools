cc32a3822f6a2e41d74b0ed2f624fe4c1b339631
laihj <hamainter@gmail.com>
2022-01-19 18:38:11 +0800

    add vuew

---
+++ src/components/DataRow.vue
@@ -0,0 +1,51 @@
+<template>
+    <tr>
+        <td>{{ rowdata[0] }}</td>
+        <td>{{ rowdata[1] }}</td>
+        <td>{{ rowdata[2] }}</td>
+        <td>{{ rowdata[3] }}</td>
+        <td>{{ rowdata[4] }}</td>
+        <td>{{ rowdata[5] }}</td>
+        <td>{{ rowdata[6] }}</td>
+        <td>{{ rowdata[7] }}</td>
+        <td>{{ rowdata[8] }}</td>
+        <td>{{ rowdata[9] }}</td>
+    </tr>
+</template>
+
+<script>
+export default {
+  name: 'DataRow',
+  props: {
+    rowdata: Array
+  },
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+td {
+    padding: 5px;
+}
+
+table tr:nth-child(odd) {
+    background: #ccc;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="hello">
+      <table border="1">
+    <tr>
+        <th>全马目标</th>
+        <th>半马目标</th>
+        <th>恢复</th>
+        <th>轻松跑A</th>
+        <th>轻松跑B</th>
+        <th>LSD</th>
+        <th>马拉松配速</th>
+        <th>力量跑</th>
+        <th>速度跑A</th>
+        <th>速度跑B</th>
+    </tr>
+    <DataRow v-for="datarow in data"
+    :key="datarow[0]"
+    :rowdata="datarow" />
+</table>
+
+  </div>
+</template>
+
+<script>
+import DataRow from './DataRow.vue'
+
+export default {
+  name: 'DataTable',
+  props: {
+    data: Array
+  },
+  components: {
+    DataRow,
+  }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+table {
+    width: 90%;
+    border-collapse: collapse;
+    margin: 0 auto;
+
+}
+
+th {
+    color: white;
+    background: #42b983;
+    padding: 5px;
+}
+
+td {
+    padding: 5px;
+}
+
+/* table tr:nth-child(odd) {
+    background: #ccc;
+} */
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
